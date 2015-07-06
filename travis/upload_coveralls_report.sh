3596853404792de439d19705fbb11820f70e3dfc
Chinmay Garde <chinmaygarde@gmail.com>
2015-07-06 14:19:44 -0700

    Update coverage script to point to the right test

--- travis/upload_coveralls_report.sh
+++ travis/upload_coveralls_report.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$COVERALLS_TOKEN" ]; then
   echo "Running coverage..."
   pub global activate dart_coveralls
-  pub global run dart_coveralls report --token "$COVERALLS_TOKEN" --debug test/cassowary_test.dart
+  pub global run dart_coveralls report --token "$COVERALLS_TOKEN" --debug test/newton_test.dart
   echo "Coverage complete..."
 else
   echo "COVERALLS_TOKEN is unset. Skipping Coverage..."
